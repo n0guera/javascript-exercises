0558fe6c720fe749a02760c92640947946740488
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2024-11-20 19:10:43 -0300

    Add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumFunc = function (x, y) {
+  let res = 0;
+  for (let i = x; i <= y; i += 1) {
+    res += i;
+  }
+  return res;
+}
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0 || typeof (x) !== "number" || typeof (y) !== "number") return 'ERROR';
 
+  let res;
+
+  x > y ? res = sumFunc(y, x) : res = sumFunc(x, y);
+
+  return res;
 };
 
 // Do not edit below this line
